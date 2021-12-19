b6429041a2d382309f409511f837ad51445a8a73
Irinel47 <cristea.irinel47@gmail.com>
2021-12-19 21:04:45 +0200

    Add project files.

---
+++ Data/Cristea_Irinel_Lab5Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cristea_Irinel_Lab5.Models;
+
+namespace Cristea_Irinel_Lab5.Data
+{
+    public class Cristea_Irinel_Lab5Context : DbContext
+    {
+        public Cristea_Irinel_Lab5Context (DbContextOptions<Cristea_Irinel_Lab5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cristea_Irinel_Lab5.Models.Book> Book { get; set; }
+
+        public DbSet<Cristea_Irinel_Lab5.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Cristea_Irinel_Lab5.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Cristea_Irinel_Lab5.Data;
+using Cristea_Irinel_Lab5.Models;
+
+namespace Cristea_Irinel_Lab5.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Cristea_Irinel_Lab5.Data.Cristea_Irinel_Lab5Context _context;
+
+        public CreateModel(Cristea_Irinel_Lab5.Data.Cristea_Irinel_Lab5Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Category.Add(Category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
